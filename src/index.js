b0c54a48694bab72febf32252358981b1facdf7a
Indrek Lasn <lasnindrek@gmail.com>
2017-07-24 13:25:43 +0300

    ssr

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-snapshot'
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+render(<App />, document.getElementById('root'));
+registerServiceWorker();
